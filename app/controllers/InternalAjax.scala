6fd8e0050a9ee4119f66d40d3ddccf34b0381981
Nicolai Dahl <ndahl89@gmail.com>
2013-04-12 11:32:19 +0200

    Added the internal ajax controller

---
+++ app/controllers/InternalAjax.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api.mvc.Action
+import play.api.mvc.BodyParsers.parse
+import java.io.File
+import play.api.mvc.Controller
+
+
+
+
+object InternalAjax extends Controller with Secured {
+  
+  def saveLeadImage = Action(parse.temporaryFile) { request =>
+    request.body.moveTo(new File("/tmp/picture"))
+    Ok("File uploaded")
+  }
+}

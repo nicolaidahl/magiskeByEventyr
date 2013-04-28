57c2692966d9efc604945ef689e7d25000c00a87
NikolajLund <nikolaj@billysbilling.com>
2013-04-28 12:40:18 +0200

    Styled modal buttons to match normal buttons and fixed an issue with the content positioning in the admin pane

--- public/js/admin.js
+++ public/js/admin.js
@@ -30,11 +30,12 @@ function adjustContentPos(){
 			audio.animate({ marginTop: ($('#approve img').height() / 2) - (audio.height() / 2)}, 200);
 			//Adjust button position
 			//Adjust yes-button position
+			
 			var yes = $('#approve #yes');
-			yes.animate({ marginTop: ($('#approve img').height() / 2) - (yes.height() / 2)}, 200);
-			//Adjust no-button position
-			var no = $('#approve #no');
-			no.animate({ marginTop: ($('#approve img').height() / 2) - (no.height() / 2)}, 200);
+			//Parse height from css (as the button is using the box-sizing attribute .height isn't accurate)
+			var yesHeight = parseFloat(yes.css('height'));
+			$(yes.closest('.row-fluid').prev('.row-fluid')[0]).animate({ paddingBottom: ($('#approve img').height() / 2) - (yesHeight / 2) }, 200);
+			
 		}, 200);
 	}
 }
